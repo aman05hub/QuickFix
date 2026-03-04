86e11c1e0f70147becdb4416394c5130390af58e
aman05hub <shiekhaman1@gmail.com>
2026-03-04 22:11:35 +0530

    update

---
+++ frontEnd/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+const Home = () => {
+    return (
+        <div className="home">
+            <div className="hero">
+                <div className="hero-text">
+                    <h1>Book Trusted Local Services Instantly</h1>
+
+                    <p>Find electricians, plumbers, cleaners and many more professional services near you. Fast booking and reliable providers.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
