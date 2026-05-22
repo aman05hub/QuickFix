429ee4abb4b29276a794afbedafe264b55a1cc63
aman05hub <shiekhaman1@gmail.com>
2026-05-23 01:47:29 +0530

    Home UI Impoved

--- frontEnd/src/pages/ChatPage.jsx
+++ frontEnd/src/pages/ChatPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect , useState } from "react";
+import { useEffect , useState , useRef} from "react";
 import { useParams  } from "react-router-dom";
 import API from "../services/api";
 import io from "socket.io-client";
